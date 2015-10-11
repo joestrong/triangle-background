a2b2f497633390722257e2e22699cb705ef69321
Joe Strong <joestrong@gmail.com>
2015-10-11 16:44:14 +0100

    Updated demo to use release v0.1.1

--- javascripts/app.js
+++ javascripts/app.js
@@ -131,7 +131,6 @@ TriangleNodeManager.prototype.restart = function(center) {
     this.queue = [0];
     this.triangles = [];
     this.calculatePositions();
-    this.drawTriangles();
 };
 
 TriangleNodeManager.prototype.calculatePositions = function() {
